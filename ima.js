9a9597fee765a7fb04f8158f50ef80819995ab2e
lipikaParvin1234567 <140397223+lipikaParvin1234567@users.noreply.github.com>
2023-09-24 12:25:04 +0600

    adding all file

---
+++ ima.js
@@ -0,0 +1,177 @@
+
+// var marks = prompt("enter your number");
+// marks = marks.toLowerCase();
+
+// if (marks=="a"|| marks=="e" ||marks== "i" ||marks== "o" ||marks== "u")
+//     console.log('vowel');
+
+// else
+
+// var m = parseInt(prompt("enter your first numver : "));
+// var n = parseInt(prompt("enter your second numver : "));
+// var sum = 0;
+// for (var x=m; x<=n; x=x+1){
+//     sum= sum+x;
+// }
+// document.write("sum =" + sum)
+
+
+// var sum = 0;
+// var i =1;
+// while( i <=100){
+//     if ( i % 3==0 && i%5==0){
+//         sum = sum+i;
+//         document.write(" " +sum);
+
+//     }
+   
+  
+//     i++;
+// }
+
+
+// var i = 7;
+// while (  i<=12 ){
+//     i++;
+//     document.write(i)
+// }
+    
+// var number = Number(prompt("enter a number"));
+    
+//   var result = number > 0 ?document.write("possistive")
+//   :number<0 ? document.write("nagative "):document.write("zero")
+
+
+
+var number = new Array();
+for(var i = 1; i<=5; i++){
+     number[i] = parseInt(prompt("inter any number"));
+}
+
+var sum = 0;
+for (var i = 1; i<=5; i++){
+    console.log(number[i])
+    sum = sum+number[i]
+}
+console.log(sum)
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+// var number = prompt("enter any number");
+// switch (number){
+//     case "a":
+//         console.log("vowel");
+//         break;
+//         case "e":
+//             console.log("vowel");
+//             break;
+//             case "i":
+//                 console.log("vowel");
+//                 break;
+//                 case "o":
+//             console.log("vowel");
+//             break;
+//             case "u":
+//                 console.log("vowel");
+//                 break;
+//                 default:
+//                     console.log("consonant")
+// }
+
+
+
+
+
+
+
+// var digit = prompt("enter any digit");
+// switch(digit) {
+//     case "0":
+//         console.log("zero");
+//         break;
+//         case "1":
+//             console.log("one")
+//             break;
+//             case "2":
+//             console.log("two")
+//             break;
+//             case "3":
+//             console.log("three")
+//             break;
+//             case "4":
+//                 console.log("four") 
+//                 break;
+//                 case "5":
+//                     console.log("five")
+//                     break;
+//                     case "6":
+//             console.log("sixx")
+//             break;
+//             case "7":
+//                 console.log("seven")
+//                 break;
+//                 case "8":
+//                     console.log("eight")
+//                     break;
+//                     case "9":
+//                         console.log("nine")
+//                         break;
+//                         default:
+//                             console.log("not a digit")
+
+            
+
+
+// var digit = prompt("enter any digit ");
+//  if (digit==0){
+//     console.log("zero")
+//  }
+//  else if (digit==1){
+//     console.log("one")
+//  }
+//  else if (digit==2){
+//     console.log("two")
+//  }
+//  else if (digit==3){
+//     console.log("three")
+//  }
+//  else if (digit==4){
+//     console.log("four")
+//  }
+//  else if (digit==5){
+//     console.log("five")
+//  }
+//  else if (digit==6){
+//     console.log("six")
+//  }
+//  else if (digit==7){
+//     console.log("seven")
+//  }
+//  else if (digit==8){
+//     console.log("eight")
+//  }
+//  else if (digit==9){
+//     console.log("nine")
+//  }
+//  else {
+//     console.log("invalid number")
+//  }
